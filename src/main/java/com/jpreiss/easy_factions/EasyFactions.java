f24590c3c69d30b5f8ea38db19480712b4fcf3fa
kard3n <jonathan.preiss@yahoo.de>
2025-12-22 19:53:47 +0100

    fix: remove unused import

--- src/main/java/com/jpreiss/easy_factions/EasyFactions.java
+++ src/main/java/com/jpreiss/easy_factions/EasyFactions.java
@@ -10,6 +10,7 @@ import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
